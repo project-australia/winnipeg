c5ecc2b0ecd402db912d744559c517655131306a
Eduardo Moroni <eduardomoroni@gmail.com>
2018-06-27 15:42:29 -0300

    feat: import signin components from native

--- src/modules/shared/components/appStatusBar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { StatusBar } from 'react-native'
-
-import { Colors } from '../../../constants'
-
-export const AppStatusBar = () => (
-  <StatusBar translucent backgroundColor={Colors.statusBar} />
-)
