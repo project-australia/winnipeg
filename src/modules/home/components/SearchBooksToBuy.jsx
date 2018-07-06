75c7769a5f416aa86eb701482b518cd8966de22d
Hebert Porto <hebertporto@gmail.com>
2018-07-06 14:24:14 -0300

    added button search, pre setup search header

--- src/modules/home/components/SearchBooksToBuy.jsx
+++ src/modules/home/components/SearchBooksToBuy.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import { Input } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Input, Button } from 'semantic-ui-react'
 
 import './styles/SearchBooksToBuy.css'
 
-const SearchBooksToBuyContainer = () => {
-  return (
-    <div>
-      <Input
-        icon={{ name: 'search', circular: true, link: true }}
-        placeholder='Search...'
-      />
-    </div>
-  )
+class SearchBooksToBuyContainer extends Component {
+  state = {
+    searchInput: ''
+  }
+
+  onChange = ({ target }) => this.setState({ [target.name]: target.value })
+
+  onSubmit = () => console.log('chama função de busca na API', this.state.searchInput)
+
+  render () {
+    const { searchInput } = this.state
+    return (
+      <div>
+        <Input
+          size='massive'
+          icon='search'
+          placeholder='Search...'
+          onChange={this.onChange}
+          value={searchInput}
+          name='searchInput'
+        />
+        <Button onClick={this.onSubmit}>Search</Button>
+      </div>
+    )
+  }
 }
 
 export const SearchBooksToBuy = SearchBooksToBuyContainer
