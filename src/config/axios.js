06cbedd723a46e16cf82289af3e6ee720838558d
Eduardo Moroni <eduardomoroni@gmail.com>
2018-07-30 08:11:50 -0300

    style: adequa a prettier

--- src/config/axios.js
+++ src/config/axios.js
@@ -4,4 +4,8 @@ export const initializeAxios = () => {
   Axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
   Axios.defaults.headers.post["Content-Type"] = "application/json";
   Axios.defaults.headers.post["Accept"] = "application/json";
+
+  if (process.env.NODE_ENV !== "production") {
+    localStorage.debug = "axios";
+  }
 };
