945261dfa8c2b61cc25c13e62cd898f1c8c9667d
Eduardo Moroni <eduardomoroni@gmail.com>
2018-07-30 08:45:25 -0300

    feat: adiciona debugger para axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -6,6 +6,22 @@ export const initializeAxios = () => {
   Axios.defaults.headers.post["Accept"] = "application/json";
 
   if (process.env.NODE_ENV !== "production") {
-    localStorage.debug = "axios";
+    attachDebugInterceptors();
   }
 };
+
+function attachDebugInterceptors() {
+  Axios.interceptors.request.use(request => {
+    console.groupCollapsed(request.url);
+    console.debug(`${request.method} request to ${request.url}, with `);
+    console.debug(request.data);
+    return request;
+  });
+
+  Axios.interceptors.response.use(response => {
+    console.debug(`Response from ${response.config.url}`);
+    console.debug({ status: response.status, ...response.data });
+    console.groupEnd();
+    return response;
+  });
+}
--- src/modules/home/components/BookListHome.jsx
+++ src/modules/home/components/BookListHome.jsx
@@ -1,68 +1,70 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { string, arrayOf } from 'prop-types'
-import Slider from 'react-slick'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { string, arrayOf } from "prop-types";
+import Slider from "react-slick";
 
-import BookItem from './BookItem'
+import BookItem from "./BookItem";
 
-import { ROUTES } from '../../router'
-import { bookProptype } from '../propTypes/book.type'
+import { ROUTES } from "../../router";
+import { bookProptype } from "../propTypes/book.type";
 
-import './styles/BookListHome.css'
-import './styles/SlickOverride.css'
+import "./styles/BookListHome.css";
+import "./styles/SlickOverride.css";
 
-import { Values } from '../../../constants/values'
+import { Values } from "../../../constants/values";
 
 export class BookListHomeContainer extends Component {
   state = {
-    screenWidth: window.innerWidth
-  }
-
-  componentDidMount = () => {
-    window.addEventListener('resize', this.updateWindowDimensions)
-  }
-
-  componentWillUnmount = () => {
-    window.removeEventListener('resize', true)
+    screenWidth: window.innerWidth,
   };
 
-  updateWindowDimensions = ({ currentTarget }) => (
-    this.setState({ screenWidth: currentTarget.innerWidth })
-  )
+  componentDidMount = () => {
+    window.addEventListener("resize", this.updateWindowDimensions);
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener("resize", true);
+  };
+
+  updateWindowDimensions = ({ currentTarget }) =>
+    this.setState({ screenWidth: currentTarget.innerWidth });
 
   setSlidesCount = () => {
-    const { screenWidth } = this.state
-    const { phoneLandscape, tablet } = Values.mediaQueries
-    console.log('screenWidth', screenWidth)
-    console.log('tablet', tablet)
-    console.log('screenWidth <= tablet', screenWidth <= tablet)
+    const { screenWidth } = this.state;
+    const { phoneLandscape, tablet } = Values.mediaQueries;
     if (screenWidth >= tablet) {
-      return 4
+      return 4;
     }
     if (screenWidth >= phoneLandscape) {
-      return 2
+      return 2;
     }
-    return 1
-  }
+    return 1;
+  };
 
-  render () {
-    const { title, books } = this.props
-    const slidesCount = this.setSlidesCount()
+  render() {
+    const { title, books } = this.props;
+    const slidesCount = this.setSlidesCount();
     const settings = {
       dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: slidesCount,
-      slidesToScroll: slidesCount
-    }
+      slidesToScroll: slidesCount,
+    };
     return (
-      <div className='blh-wrapper'>
-        <div className='blh-title-box'>
-          <span className='title'>{title}</span>
-          <span><Link to={{
-            pathname: ROUTES.BOOKLIST,
-            state: { type: 'ALL' }
-          }}>View All</Link></span>
+      <div className="blh-wrapper">
+        <div className="blh-title-box">
+          <span className="title">{title}</span>
+          <span>
+            <Link
+              to={{
+                pathname: ROUTES.BOOKLIST,
+                state: { type: "ALL" },
+              }}
+            >
+              View All
+            </Link>
+          </span>
         </div>
         {/* <div className='blh-book-list'> */}
         <Slider {...settings}>
@@ -70,13 +72,13 @@ export class BookListHomeContainer extends Component {
         </Slider>
         {/* </div> */}
       </div>
-    )
+    );
   }
 }
 
 BookListHomeContainer.propTypes = {
   books: arrayOf(bookProptype).isRequired,
-  title: string.isRequired
-}
+  title: string.isRequired,
+};
 
-export const BookListHome = BookListHomeContainer
+export const BookListHome = BookListHomeContainer;
