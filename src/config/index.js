5617dbf18eb3d3755affbfcdc866204475d509f1
Eduardo Moroni <eduardomoroni@gmail.com>
2018-08-07 15:57:32 -0300

    feat: remove dotenv

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,8 @@
-import DotEnv from "dotenv";
 import { initializePaypalSDK } from "./paypal";
 import { initializeFirebase } from "./firebase";
 import { initializeAxios } from "./axios";
 
 export const setUpConfigs = () => {
-  DotEnv.config();
   initializePaypalSDK();
   initializeFirebase();
   initializeAxios();
